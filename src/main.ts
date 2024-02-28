ab24a03376ec94fb6199e8a42ea986212f3e81f0
Fyodor Urlapov <urlapov-fyodor@mail.ru>
2024-02-28 12:16:18 +0300

    - add author and node version
    - add ports configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get("PORT") || 2024
+  await app.listen(port);
 }
 bootstrap();
