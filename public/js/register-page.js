b55c3d146ed7bd1439404e91a44889106ef4cb96
Fyodor Urlapov <urlapov-fyodor@mail.ru>
2024-05-12 15:47:14 +0300

    feat: Restyling registration page

--- public/js/register-page.js
+++ public/js/register-page.js
@@ -16,3 +16,30 @@ async function signup(event) {
     alert('Account with this nickname already exists');
   }
 }
+
+window.addEventListener('load', () => {
+  const photoInput = document.getElementById('photoInput');
+  const uploadButton = document.getElementById('uploadButton');
+  photoInput.addEventListener('change', function () {
+    // If a file is selected, change the text of the button
+    if (photoInput.files.length > 0) {
+      uploadButton.textContent = 'Photo uploaded \u2713';
+      uploadButton.style.backgroundColor = '#3ABE46';
+    }
+  });
+
+  uploadButton.addEventListener('click', function () {
+    photoInput.click();
+  });
+
+  uploadButton.addEventListener('mouseover', function () {
+    if (photoInput.files.length === 0) {
+      uploadButton.style.backgroundColor = '#bbc1c5';
+    }
+  });
+  uploadButton.addEventListener('mouseout', function () {
+    if (photoInput.files.length === 0) {
+      uploadButton.style.backgroundColor = '#2c89ed';
+    }
+  });
+});
