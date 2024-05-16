0dd6615bc7cd7309ffc56c5ba536c62e6f32c73d
Fyodor Urlapov <urlapov-fyodor@mail.ru>
2024-05-16 15:29:11 +0300

    feat: add default avatar to navbar

--- public/js/friends-page.js
+++ public/js/friends-page.js
@@ -1,14 +1,3 @@
 async function redirectToProfile(nickname) {
   window.location.href = `/users/${nickname}`;
 }
-
-window.onload = function () {
-  const defaultImage = '../objects/default-avatar-2-shifted.png';
-  const avatarImages = document.querySelectorAll('img.avatar');
-  avatarImages.forEach((image) => {
-    console.log(image.getAttribute('src'));
-    if (image.getAttribute('src') === '') {
-      image.src = defaultImage;
-    }
-  });
-};
