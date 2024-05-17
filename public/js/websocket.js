501a9aa01c101df6f76ba85581590fa2192777fd
Fyodor Urlapov <urlapov-fyodor@mail.ru>
2024-05-17 15:45:05 +0300

    feat: restyling footer

--- public/js/websocket.js
+++ public/js/websocket.js
@@ -1,14 +1,16 @@
 const socket = io(window.location.origin);
 
-var friendRequestStateChanged = (changerNickname, changeeNickname) => {
-  payload = { changerNickname, changeeNickname };
+const friendRequestStateChanged = (changerNickname, changeeNickname) => {
+  const payload = { changerNickname, changeeNickname };
   socket.emit('friend request', payload);
 };
 
 socket.on('friend request result', (changerNickname, changeeNickname) => {
-  var userProfileLink = document.getElementById('link-to-user-profile-navbar');
-  var href = userProfileLink.getAttribute('href');
-  var pageNickname = href.split('/').pop();
+  const userProfileLink = document.getElementById(
+    'link-to-user-profile-navbar',
+  );
+  const href = userProfileLink.getAttribute('href');
+  const pageNickname = href.split('/').pop();
 
   if (pageNickname === changeeNickname || pageNickname === changerNickname) {
     const message = `User ${changerNickname} has changed the state 
