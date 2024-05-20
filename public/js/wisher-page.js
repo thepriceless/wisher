f86c253fe027cd8f4d40dc9b1dd67a77c506241e
Fyodor Urlapov <urlapov-fyodor@mail.ru>
2024-05-20 18:59:06 +0300

    fix: change links on wisher-page when wishitem is changed

--- public/js/wisher-page.js
+++ public/js/wisher-page.js
@@ -11,5 +11,32 @@ function wisherGetNextItem() {
       const wisherItemLink = data.imageLink;
       const imageElement = document.getElementById('wisher-item-image');
       imageElement.src = wisherItemLink;
+
+      const linksContainer = document.querySelector('.links-container');
+      const existingLinks = linksContainer.querySelectorAll(
+        '.suggestion-zone__marketplace-single-link',
+      );
+
+      const helperText = document.querySelector('.helper-text');
+      helperText.style.display =
+        data.itemshopLinks.length > 0 ? 'block' : 'none';
+
+      data.itemshopLinks.forEach((link, index) => {
+        let aTag;
+        if (index < existingLinks.length) {
+          aTag = existingLinks[index];
+        } else {
+          aTag = document.createElement('a');
+          aTag.className = 'suggestion-zone__marketplace-single-link';
+          aTag.target = '_blank';
+          linksContainer.appendChild(aTag);
+        }
+        aTag.href = link;
+        aTag.textContent = `Link №${index + 1}`;
+      });
+
+      for (let i = data.itemshopLinks.length; i < existingLinks.length; i++) {
+        existingLinks[i].remove();
+      }
     });
 }
